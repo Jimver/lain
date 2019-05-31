216eed58294f61c34b814bde97adce085fcea6be
jimver <jim.verheijde@hotmail.com>
2019-05-31 23:48:41 +0200

    Fix flicker on Awesome restart

--- widget/contrib/redshift.lua
+++ widget/contrib/redshift.lua
@@ -8,7 +8,7 @@
 
 local async   = require("lain.helpers").async
 local awful   = require("awful")
-local execute = os.execute
+local execute = awful.spawn.easy_async
 local type    = type
 
 -- Redshift
@@ -16,21 +16,21 @@ local type    = type
 local redshift = { active = false, pid = nil }
 
 function redshift:start()
-    execute("pkill redshift")
-    awful.spawn.with_shell("redshift -x") -- clear adjustments
-    redshift.pid = awful.spawn.with_shell("redshift")
-    redshift.active = true
-    if type(redshift.update_fun) == "function" then
-        redshift.update_fun(redshift.active)
-    end
+    execute("pkill redshift", function() 
+        awful.spawn.with_shell("redshift -x") -- clear adjustments
+        redshift.pid = awful.spawn.with_shell("redshift")
+        redshift.active = true
+        if type(redshift.update_fun) == "function" then
+            redshift.update_fun(redshift.active)
+        end
+    end)
 end
 
 function redshift:toggle()
     async({ awful.util.shell, "-c", string.format("ps -p %d -o pid=", redshift.pid) }, function(f)
         if f and #f > 0 then -- redshift is running
             -- Sending -USR1 toggles redshift (See project website)
-            execute("pkill -USR1 redshift")
-            redshift.active = not redshift.active
+            execute("pkill -USR1 redshift", function() redshift.active = not redshift.active end)
         else -- not started or killed, (re)start it
             redshift:start()
         end
@@ -45,11 +45,12 @@ end
 --                 Use it to update widget text or icons on status change.
 function redshift:attach(widget, fun)
     redshift.update_fun = fun or function() end
-    execute("pkill -9 redshift")
-    if not redshift.pid then redshift:start() end
-    if widget then
-        widget:buttons(awful.util.table.join(awful.button({}, 1, function () redshift:toggle() end)))
-    end
+    execute("pkill -9 redshift", function() 
+        if not redshift.pid then redshift:start() end
+        if widget then
+            widget:buttons(awful.util.table.join(awful.button({}, 1, function () redshift:toggle() end)))
+        end       
+    end)
 end
 
 return redshift
